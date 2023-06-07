935855696977161652e32bb318065556b79adb77
[SW4/2반] 유하영 <518hayoung@gmail.com>
2023-06-07 11:02:37 +0000

    Update index.js friend라우터 추가

--- routers/index.js
+++ routers/index.js
@@ -14,6 +14,7 @@ router.get("/", (req, res) => {
 });
 router.use("/user", userRouter);
 router.use("/auth", authRouter);
+router.use("/friend", friendRouter);
 router.use("/adminUser", getUserFromJwt, adminUserRouter);
 router.use("/answer", answerRouter);
 router.use("/answer/:answerId/comment", commentRouter);
